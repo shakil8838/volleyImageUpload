0ff95b682d465bbbd7d7a298b68d23c19fe12e55
shakil8838 <eng.shakil8838@gmail.com>
2019-11-21 18:58:31 +0600

    Completed

---
+++ app/src/main/java/xyz/xandsoft/volleyimageupload/Base64Activity.java
@@ -0,0 +1,14 @@
+package xyz.xandsoft.volleyimageupload;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Base64Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base64);
+    }
+}
---
+++ app/src/main/java/xyz/xandsoft/volleyimageupload/MultiPartActivity.java
@@ -0,0 +1,14 @@
+package xyz.xandsoft.volleyimageupload;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MultiPartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_part);
+    }
+}
